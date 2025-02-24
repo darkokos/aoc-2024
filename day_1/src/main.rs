5a29509233f80b640c954450fcfc1dc7852a6953
darkokos <darkosvilar02@gmail.com>
2025-02-24 05:08:19 +0100

    [add] first day solutions

---
+++ day_1/src/main.rs
@@ -0,0 +1,55 @@
+use std::{collections::HashMap, fs};
+
+const COLUMN_DELIMITER: &str = "   ";
+
+fn get_input() -> String {
+    fs::read_to_string("input.txt").expect("The file should have been read")
+}
+
+fn a(input: &str) {
+    let mut first_list = Vec::with_capacity(1000);
+    let mut second_list = Vec::with_capacity(1000);
+    for line in input.lines() {
+        let mut numbers = line.split(COLUMN_DELIMITER);
+
+        first_list.push(numbers.next().unwrap().parse::<u32>().unwrap());
+        second_list.push(numbers.next().unwrap().parse::<u32>().unwrap());
+    }
+
+    first_list.sort_unstable();
+    second_list.sort_unstable();
+
+    let mut result: u32 = 0;
+    for pair in first_list.iter().zip(second_list) {
+        result += pair.0.abs_diff(pair.1);
+    }
+
+    println!("{result}");
+}
+
+fn b(input: &str) {
+    let mut first_list = Vec::with_capacity(1000);
+    let mut second_list_histogram = HashMap::new();
+    for line in input.lines() {
+        let mut numbers = line.split(COLUMN_DELIMITER);
+
+        first_list.push(numbers.next().unwrap());
+        second_list_histogram
+            .entry(numbers.next().unwrap())
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    }
+
+    let mut result: u32 = 0;
+    for number in first_list {
+        result += number.parse::<u32>().unwrap() * second_list_histogram.get(number).unwrap_or(&0);
+    }
+
+    println!("{result}");
+}
+
+fn main() {
+    let input = get_input();
+    a(&input);
+    b(&input);
+}
